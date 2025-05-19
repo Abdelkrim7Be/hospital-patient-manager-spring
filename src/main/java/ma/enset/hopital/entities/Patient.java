86d7680b798829308ffed45a61dff0bc192bb460
Abdelkrim7Be <119794076+Abdelkrim7Be@users.noreply.github.com>
2025-05-19 02:30:46 +0100

    Patient.html Updated & Form Validation Enhanced

--- src/main/java/ma/enset/hopital/entities/Patient.java
+++ src/main/java/ma/enset/hopital/entities/Patient.java
@@ -1,9 +1,7 @@
 package ma.enset.hopital.entities;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -11,17 +9,23 @@ import java.util.Date;
 
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
-
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotEmpty
-    @Size(min = 3, max = 50)
+    
+    @NotEmpty(message = "Le nom ne peut pas être vide")
+    @Size(min = 3, max = 50, message = "Le nom doit contenir entre 3 et 50 caractères")
     private String name;
+    
+    @NotNull(message = "La date de naissance est obligatoire")
+    @Past(message = "La date de naissance doit être dans le passé")
     @Temporal(TemporalType.DATE)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date birthday;
+    
     private boolean isSick;
-    @DecimalMin("0")
+    
+    @DecimalMin(value = "0", message = "Le score doit être positif")
+    @Max(value = 100, message = "Le score maximum est 100")
     private int score;
 }
